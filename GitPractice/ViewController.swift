573ec90b6639ea8d2c80f6dc326da91336b11509
Jay Modhawadiya <jay.modhawadiya@gmail.com>
2022-01-10 23:20:21 +0530

    Variable Defined

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -11,7 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var a = 1
+        var b = 2
+        var c = 3
+        var d = 4
+        var e = 5
+        var f = 6
+        var g = 123
+        var h = 123
         // Do any additional setup after loading the view.
+        
     }
 
 
